a12d16596f6ac0d375bae5e38124303b617b5c18
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-10-07 18:02:37 +0200

    mini project w2d1 Ahmed and Olga

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,45 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+    return this.currentQuestionIndex;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    for (let i = 0; i < this.questions.length; i++) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+    return this.questions;
+  }
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    if (this.getQuestion().answer === answer) {
+      this.correctAnswers += 1;
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 }
